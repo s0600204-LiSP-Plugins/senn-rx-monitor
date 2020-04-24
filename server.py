2e1fedc71f1e68f7b861877638b726772814a9bb
s0600204 <dev@s0600204>
2020-04-24 18:21:57 +0100

    Permit registering callbacks and dispatching to them
    It is now possible to register callbacks on particular IP addresses. Thus, when
    a message is received from a registered IP address, it is dispatched to the
    appropriate place.

--- server.py
+++ server.py
@@ -22,40 +22,73 @@
 # You should have received a copy of the GNU General Public License
 # along with Linux Show Player.  If not, see <http://www.gnu.org/licenses/>.
 
+import logging
 import socket
 from socketserver import BaseRequestHandler, UDPServer
 from threading import Thread
 
 from lisp.core.util import get_lan_ip
 
+logger = logging.getLogger(__name__)
+
 PORT = 53212
 
 class SennheiserUDPHandler(BaseRequestHandler):
-    def handle(self):
-        print(self.request)
-        messages = self.request[0].strip().split('\r')
-        #socket = self.request[1]
-        # Do something with the data here
-        # Hint: split on "\r" to get seperate messages
-        for msg in messages:
-            print(str(msg, 'ascii'))
 
+    def handle(self):
+        for msg in str(self.request[0].strip(), 'ascii').split('\r'):
+            msg = msg.split()
+            self.server.dispatch(self.client_address[0], msg[0], msg[1:])
 
 class SennheiserUDPListener(Thread):
 
     def __init__(self):
         super().__init__(daemon=True)
-        ip = get_lan_ip()
-        self._server = UDPServer((ip, PORT), SennheiserUDPHandler)
+        self.ip = get_lan_ip()
+        self._server = SennheiserUDPServer((self.ip, PORT), SennheiserUDPHandler)
+
+    def deregister(self, ip):
+        return self._server.deregister(ip)
+
+    def register(self, ip, callback):
+        return self._server.register(ip, callback)
 
     def run(self):
         with self._server as server:
+            logger.info("Listening for messages from Sennheiser radio-mic receivers on UDP {}:{}".format(self.ip, PORT))
             server.serve_forever()
 
     def stop(self):
         if self.is_alive():
             self._server.shutdown()
             self.join()
+
+
+class SennheiserUDPServer(UDPServer):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._registered = {}
+
+    def deregister(self, ip):
+        if ip not in self._registered:
+            logger.warning("Unable to deregister device on {}, due to not being registered".format(ip))
+            return False
+
+        del self._registered[ip]
+        return True
+
+    def dispatch(self, source, command, attributes=[]):
+        if source in self._registered:
+            self._registered[source](command, attributes)
+
+    def register(self, ip, callback):
+        if ip in self._registered:
+            logging.warning("Unable to register device on {}, due to a device already being registered at this address".format(ip))
+            return False
+
+        self._registered[ip] = callback
+        return True
 
 
 class SennheiserUDPTalker:
